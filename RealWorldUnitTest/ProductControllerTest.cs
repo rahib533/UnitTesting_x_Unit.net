26b056eb8a22c0ce1733e1c5076800cc872b4b0a
= <=>
2021-12-23 11:32:41 +0400

    23 Dec 2021 InMemoryDB

---
+++ RealWorldUnitTest/ProductControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorldUnitTest
+{
+    public class ProductControllerTest
+    {
+        protected DbContextOptions<NorthwindContext> Options { get; private set; }
+
+        public void SetContextOptions(DbContextOptions<NorthwindContext> options)
+        {
+            Options = options;
+        }
+
+        public void SeedData()
+        {
+            using (NorthwindContext northwindContext = new NorthwindContext(Options))
+            {
+                northwindContext.Database.EnsureDeleted();
+                northwindContext.Database.EnsureCreated();
+
+                northwindContext.Categories.Add(new Category { CategoryId = 1, CategoryName = "Test 1", Description = "test 1 desc" });
+                northwindContext.Categories.Add(new Category { CategoryId = 2, CategoryName = "Test 2", Description = "test 2 desc" });
+                northwindContext.SaveChanges();
+
+                northwindContext.Products.Add(new Product { ProductId = 1, ProductName = "Product 1", UnitPrice = 5, CategoryId = 1, UnitsInStock = 7 });
+                northwindContext.Products.Add(new Product { ProductId = 2, ProductName = "Product 2", UnitPrice = 5, CategoryId = 1, UnitsInStock = 7 });
+                northwindContext.Products.Add(new Product { ProductId = 3, ProductName = "Product 3", UnitPrice = 5, CategoryId = 2, UnitsInStock = 73 });
+                northwindContext.Products.Add(new Product { ProductId = 4, ProductName = "Product 4", UnitPrice = 5, CategoryId = 2, UnitsInStock = 27 });
+                northwindContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ RealWorldUnitTest/ProductControllerTestWithInMemoryData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Project.Controllers;
+using MVC.Project.Models;
+using MVC.Project.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RealWorldUnitTest
+{
+    public class ProductControllerTestWithInMemoryData : ProductControllerTest
+    {
+        public ProductControllerTestWithInMemoryData()
+        {
+            SetContextOptions(new DbContextOptionsBuilder<NorthwindContext>().UseInMemoryDatabase("NorthwindInMemoryDB").Options);
+            SeedData();
+        }
+
+        [Fact]
+        public async Task Create_ModelValidProduct_ReturnRedirectToActionWithSaveProduct()
+        {
+            using (NorthwindContext northwindContext = new NorthwindContext(Options))
+            {
+                var category = northwindContext.Categories.First();
+
+                var product = new Product { ProductId = 6, ProductName = "Product Create", UnitPrice = 5, CategoryId = 1, UnitsInStock = 7 };
+                var controller = new ProductsController(new Repository<Product>(northwindContext), new Repository<Category>(northwindContext), new Repository<Supplier>(northwindContext));
+
+                var result = await controller.Create(product);
+                var redirect = Assert.IsType<RedirectToActionResult>(result);
+
+                Assert.Equal("Index", redirect.ActionName);
+            }
+        }
+    }
+}
